0e53ce81b4041e47ea041e60a42c23e2f671509a
SAYANDEEP2004 <94121243+SAYANDEEP2004@users.noreply.github.com>
2021-11-11 19:38:59 +0530

    Create m2.py

---
+++ m2.py
@@ -0,0 +1,8 @@
+principal=float(input("enter the principal: "))
+rate=float(input("enter the interest rate: "))
+time=float(input("enter the time : "))
+
+si=principal*rate*time*0.01
+
+print("the si is: ",si)
+print("the amount is: ",principal+si) 
